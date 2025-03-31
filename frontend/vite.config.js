1755404c85d36d9e20de417a20919635083f203b
YukichiOhno <juuzousuzuya200@gmail.com>
2025-03-30 23:03:13 -0500

    Initialized Backend and Frontend Servers

---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import dotenv from 'dotenv';
+import removeConsole from 'vite-plugin-remove-console'
+
+dotenv.config();
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    removeConsole()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: parseInt(process.env.VITE_FRONTEND_PORT), 
+  },
+  preview: {
+    port: parseInt(process.env.VITE_FRONTEND_PORT)
+  },
+})
